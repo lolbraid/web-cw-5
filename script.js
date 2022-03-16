4aae66890fb63d7a4bb976688fa0e6abe9636587
lolbraid <lolbraid@gmail.com>
2022-03-16 19:45:39 +0300

    part 2

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-alert('عبد العزيز ياسر')
-console.log(17)
+alert('عبد العزيز ياسر');
+console.log(17);
 // change log color
-console.warn('أنا في الصف 12')
+console.warn('أنا في الصف 12');
 
 
 // جافاسكربت ليست جافا
+
+
+let budget = 20; 
+console.info(80 >= budget && budget >= 30)
+console.log('---------------------------------------------------------')
+budget += 30
+console.info(80 >= budget && budget >= 30)
